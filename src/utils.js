0c206288febcd06f5efd20021438507fbdc1040d
Seth44 <rauchseth@gmail.com>
2020-03-05 23:45:17 -0700

    Revert "Updates"
    This reverts commit 7cc8042b129397a209cc24c77c20f468d3b6290c.

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+export function getRandomDate() {
+    const todaysYear = new Date().getFullYear() + 1;
+    const randomYear = getRandomInt(2015, todaysYear);
+    const randomDay = getRandomInt(1, 29);
+    const randomMonth = getRandomInt(1, 12);
+
+    return `${randomYear}-${randomMonth}-${randomDay}`;
+}
+
+export function getRandomVideoId(items) {
+    const randomNum = getRandomInt(0, 50);
+    return items[randomNum].id.videoId;
+}
