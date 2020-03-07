7185a6e8b11c89cd701b862fce8a9dfb87a1fda2
Seth44 <rauchseth@gmail.com>
2020-03-07 14:34:55 -0700

    Updates

---
+++ precache-manifest.1b930c6812fc3afa16f1010fc9ed5aaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b160535b44d85983c048e43cabbc16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32adb11d9bcc71796e48",
+    "url": "/static/css/main.2d67727d.chunk.css"
+  },
+  {
+    "revision": "600bb40a5e8935bc994d",
+    "url": "/static/js/2.bc1230ca.chunk.js"
+  },
+  {
+    "revision": "f3f414ffe4912a78b81b335cf7d0e117",
+    "url": "/static/js/2.bc1230ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32adb11d9bcc71796e48",
+    "url": "/static/js/main.cae0656e.chunk.js"
+  },
+  {
+    "revision": "61c9d2075d4063e53e4a",
+    "url": "/static/js/runtime-main.52a73f12.js"
+  },
+  {
+    "revision": "fd81c1f7acc541dcf6355d350d8d05b5",
+    "url": "/static/media/Funhaus_circle_logo.fd81c1f7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FunhausTV/precache-manifest.81cb74f76097e8936a02d288f83943d5.js"
+  "/precache-manifest.1b930c6812fc3afa16f1010fc9ed5aaf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FunhausTV/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
